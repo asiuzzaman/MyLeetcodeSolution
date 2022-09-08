17015b74321b7a84994f8b96f7eeee42ed117f0c
asiuzzaman <zaman1304006@gmail.com>
2022-09-08 23:48:12 +0600

    Leetcode 230: Kth smallest Element in BST with file

---
+++ 230/kthSmallestElementInBST.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+//IDEA: When traversing BST in inorder way it will automatically store the value in sorted order.
+
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<int>res;
+    void inorder(TreeNode * root) {
+        if( root == NULL) return;
+        
+        inorder(root->left);
+        res.push_back(root->val);
+        inorder(root->right);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root);
+       return res[k-1];
+    }
+};
+
+int main() {
+    Solution s;
+}
